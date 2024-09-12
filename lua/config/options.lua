d8eee0ee3505f7399e2cb206ca6626242254f0bc
Saalim Aqueel <aqueel192@gmail.com>
2024-09-12 07:36:28 +0530

    mason lsp config

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,7 @@
 -- File: lua/config/options.lua
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -36,4 +36,34 @@ return {
         require("config.plugins.lualine")
       end,
     },
-  }
+-- Mason Setup
+    {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup({
+        ui = {
+          icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗"
+          }
+        }
+      })
+    end
+  },
+  {
+    'williamboman/mason-lspconfig.nvim',
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {"lua_ls"},
+        automatic_installation = true
+      })
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("config.plugins.lspconfig")
+    end
+  },
+}
