f29d634ca4efbcaebb40246c9abef01f25d0ee06
Saalim Aqueel <aqueel192@gmail.com>
2024-09-15 22:27:39 +0530

    fixes

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -1,170 +1,184 @@
 return {
-	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-	{
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.8",
-		branch = "0.1.x",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"nvim-telescope/telescope-ui-select.nvim",
-		},
-		config = function()
-			require("config.plugins.telescope")
-		end,
-	},
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		config = function()
-			require("config.plugins.treesitter")
-		end,
-	},
-	{
-		"nvim-neo-tree/neo-tree.nvim",
-		branch = "v3.x",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"nvim-tree/nvim-web-devicons",
-			"MunifTanjim/nui.nvim",
-		},
-		config = function()
-			require("config.plugins.neo-tree")
-		end,
-	},
-	{
-		"nvim-lualine/lualine.nvim",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		config = function()
-			require("config.plugins.lualine")
-		end,
-	},
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup({
-				ui = {
-					icons = {
-						package_installed = "✓",
-						package_pending = "➜",
-						package_uninstalled = "✗",
-					},
-				},
-			})
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		dependencies = { "williamboman/mason.nvim" },
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"lua_ls",
-					"ts_ls",
-					"pyright",
-				},
-				automatic_installation = true,
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		dependencies = {
-			"williamboman/mason.nvim",
-			"williamboman/mason-lspconfig.nvim",
-		},
-		config = function()
-			require("config.plugins.lspconfig")()
-		end,
-	},
-	{
-		"nvimtools/none-ls.nvim",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"nvimtools/none-ls-extras.nvim",
-		},
-		config = function()
-			require("config.plugins.none-ls")
-		end,
-	},
-	{
-		"goolord/alpha-nvim",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		config = function()
-			require("config.plugins.alpha")
-		end,
-	},
-	{
-		"ellisonleao/gruvbox.nvim",
-		priority = 1000,
-		config = function()
-			vim.cmd("colorscheme gruvbox")
-		end,
-	},
-	-- Autocompletions with Luasnip
-	{
-		{ "hrsh7th/cmp-nvim-lsp" },
-		{
-			"L3MON4D3/LuaSnip",
-			dependencies = {
-				"saadparwaiz1/cmp_luasnip",
-				"rafamadriz/friendly-snippets",
-			},
-		},
-		{
-			"hrsh7th/nvim-cmp",
-			config = function()
-				require("config.plugins.autocompletions")
-			end,
-		},
-	},
-	-- DAP
-	{
-		"mfussenegger/nvim-dap",
-		dependencies = {
-			"nvim-neotest/nvim-nio",
-			"rcarriga/nvim-dap-ui",
-			"leoluz/nvim-dap-go",
-			"mxsdev/nvim-dap-vscode-js",
-			{
-				"microsoft/vscode-js-debug",
-				build = "npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out",
-			},
-		},
-		config = function()
-			require("config.plugins.debugging")
-		end,
-	},
-	{
-		"nvim-pack/nvim-spectre",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-		},
-		config = function()
-			require("config.plugins.spectre")
-		end,
-	},
-	-- ToggleTerm
-	{
-		"akinsho/toggleterm.nvim",
-		version = "*",
-		config = function()
-			require("config.plugins.toggle-term")
-		end,
-	},
-	{
-		{
-			"folke/which-key.nvim",
-			config = function()
-				require("config.plugins.which-key")
-			end,
-		},
-	},
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
   {
-		'akinsho/bufferline.nvim',
-		version = "*",
-		dependencies = 'nvim-tree/nvim-web-devicons',
-		config = function()
-			require("config.plugins.bufferline")
-		end,
-	},
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.8",
+    branch = "0.1.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope-ui-select.nvim",
+    },
+    config = function()
+      require("config.plugins.telescope")
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require("config.plugins.treesitter")
+    end,
+  },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "MunifTanjim/nui.nvim",
+    },
+    config = function()
+      require("config.plugins.neo-tree")
+    end,
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("config.plugins.lualine")
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup({
+        ui = {
+          icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗",
+          },
+        },
+      })
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = { "williamboman/mason.nvim" },
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+          "ts_ls",
+          "pyright",
+        },
+        automatic_installation = true,
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+    },
+    config = function()
+      require("config.plugins.lspconfig")()
+    end,
+  },
+  {
+    "nvimtools/none-ls.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvimtools/none-ls-extras.nvim",
+    },
+    config = function()
+      require("config.plugins.none-ls")
+    end,
+  },
+  {
+    "goolord/alpha-nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("config.plugins.alpha")
+    end,
+  },
+  {
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000,
+    config = function()
+      vim.cmd("colorscheme gruvbox")
+    end,
+  },
+  -- Autocompletions with Luasnip
+  {
+    { "hrsh7th/cmp-nvim-lsp" },
+    {
+      "L3MON4D3/LuaSnip",
+      dependencies = {
+        "saadparwaiz1/cmp_luasnip",
+        "rafamadriz/friendly-snippets",
+      },
+    },
+    {
+      "hrsh7th/nvim-cmp",
+      config = function()
+        require("config.plugins.autocompletions")
+      end,
+    },
+  },
+  -- DAP
+  {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+      "nvim-neotest/nvim-nio",
+      "rcarriga/nvim-dap-ui",
+      "leoluz/nvim-dap-go",
+      "mxsdev/nvim-dap-vscode-js",
+      {
+        "microsoft/vscode-js-debug",
+        build = "npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out",
+      },
+    },
+    config = function()
+      require("config.plugins.debugging")
+    end,
+  },
+  {
+    "nvim-pack/nvim-spectre",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    config = function()
+      require("config.plugins.spectre")
+    end,
+  },
+  -- ToggleTerm
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    config = function()
+      require("config.plugins.toggle-term")
+    end,
+  },
+  {
+    "echasnovski/mini.nvim",
+    version = false,
+    config = function()
+      require("mini.icons").setup()
+    end,
+  },
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    init = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 300
+    end,
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+    },
+    config = function()
+      require("config.plugins.which-key")
+    end,
+  },
+  {
+    "akinsho/bufferline.nvim",
+    version = "*",
+    dependencies = "nvim-tree/nvim-web-devicons",
+    config = function()
+      require("config.plugins.bufferline")
+    end,
+  },
 }
--- lua/config/plugins/which-key.lua
+++ lua/config/plugins/which-key.lua
@@ -1,32 +1,43 @@
--- config/plugins/which-key.lua
+-- File: lua/config/plugins/which-key.lua
+
 local wk = require("which-key")
 
-wk.setup {}
+wk.setup {
+    icons = {
+        breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
+        separator = "➜", -- symbol used between a key and its label
+        group = "+", -- symbol prepended to a group
+    },
+    window = {
+        border = "single", -- none, single, double, shadow
+        position = "bottom", -- bottom, top
+    },
+}
 
 -- Register custom keybindings
 wk.register({
-  ["<C-a>"] = { "ggVG", "Select All" },
-  ["<C-c>"] = { '"+y', "Copy", mode = { "n", "v" } },
-  ["<C-v>"] = { '"+p', "Paste", mode = "n" },
-  ["<C-v>"] = { '<C-r>+', "Paste", mode = "i" },
-  ["<C-v>"] = { '<C-r>+', "Paste", mode = "c" },
-  ["<C-S-c>"] = { '"+y', "Copy in Visual Mode", mode = "v" },
-  ["<C-S-v>"] = { '"+p', "Paste", mode = "n" },
-  ["<C-S-v>"] = { '"+p', "Paste", mode = "v" },
+    ["<C-a>"] = { "ggVG", "Select All" },
+    ["<C-c>"] = { '"+y', "Copy" },
+    ["<C-v>"] = { '"+p', "Paste" },
+    ["<C-S-v>"] = { '"+p', "Paste" },
+    ["<C-s>"] = { ":w<CR>", "Save" }, -- Save command in normal mode
 }, { mode = "n" })
 
 wk.register({
-  ["<C-c>"] = { '"+y', "Copy" },
-  ["<C-v>"] = { '"+p', "Paste" },
-  ["<C-S-c>"] = { '"+y', "Copy" },
-  ["<C-S-v>"] = { '"+p', "Paste" },
+    ["<C-c>"] = { '"+y', "Copy" },
+    ["<C-v>"] = { '"+p', "Paste" },
+    ["<C-S-c>"] = { '"+y', "Copy" },
+    ["<C-S-v>"] = { '"+p', "Paste" },
+    ["<C-s>"] = { ":w<CR>", "Save" }, -- Save command in visual mode
 }, { mode = "v" })
 
 wk.register({
-  ["<C-v>"] = { '<C-r>+', "Paste" },
+    ["<C-v>"] = { "<C-r>+", "Paste" },
+    ["<C-s>"] = { "<Cmd>w<CR>", "Save" }, -- Save command in insert mode
 }, { mode = "i" })
 
 wk.register({
-  ["<C-v>"] = { '<C-r>+', "Paste" },
+    ["<C-v>"] = { "<C-r>+", "Paste" },
+    ["<C-s>"] = { "<Cmd>w<CR>", "Save" }, -- Save command in command-line mode
 }, { mode = "c" })
 
